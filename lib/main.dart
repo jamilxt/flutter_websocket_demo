e730a5a875c023947d82554377aafaa25891e7d1
Jamilur Rahman <jamilur.rahman@bs-23.net>
2021-06-17 00:26:17 +0600

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:stomp_dart_client/stomp.dart';
+import 'package:stomp_dart_client/stomp_config.dart';
+import 'package:stomp_dart_client/stomp_frame.dart';
+
+// If Server is using SockJs
+// final socketUrl = 'http://10.0.2.2:8080/ws-message';
+// If Server is not using SockJs
+final socketUrl = 'ws://10.0.2.2:8080/ws-message';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WebSocket Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter WebSocket Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late StompClient stompClient;
+  String message = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    stompClient = StompClient(
+        config: StompConfig(
+      url: socketUrl,
+      onConnect: onConnect,
+      onWebSocketError: (dynamic error) => print(error.toString()),
+    ));
+
+    stompClient.activate();
+  }
+
+  onConnect(StompFrame frame) {
+    stompClient.subscribe(
+        destination: '/topic/message',
+        callback: (StompFrame frame) {
+          if (frame.body != null) {
+            Map<String, dynamic> result = json.decode(frame.body.toString());
+            setState(() => message = result['message']);
+          }
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Your message from server:'),
+            Text(
+              '$message',
+              style: Theme.of(context).textTheme.bodyText1,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    stompClient.deactivate();
+    super.dispose();
+  }
+}
